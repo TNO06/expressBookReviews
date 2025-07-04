08b8b22b0db657fd76344850eca35bb0f85224af
TNO06 <dhawalkashyap5399@gmail.com>
2025-07-04 16:33:28 -0400

    upates

--- final_project/index.js
+++ final_project/index.js
@@ -5,7 +5,7 @@ const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
 const app = express();
-const PORT = 5000;
+const PORT = 5001;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
